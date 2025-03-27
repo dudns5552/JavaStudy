dbc4464a7020c11cffe7a750b709b20ee5e48fba
dudns5552 <dudns5552@gmail.com>
2025-03-27 17:48:28 +0900

    HelloWorld 프린트문 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,12 @@
 
 public class HelloWorld {
+	
 	public static void main(String[] args) {
 	
 		System.out.println("이클립스 첫 실행");
 		System.out.println("Hello Eclipse");
 		
+		System.out.println("Github 연동완료");
 	}
 }
 
