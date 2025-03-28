44a647597ef3d202d6ee4d2da94b81822f59757c
dudns5552 <dudns5552@gmail.com>
2025-03-28 17:09:31 +0900

    250328 1709

--- src/ex20io/E11ObjectSerializable.java
+++ src/ex20io/E11ObjectSerializable.java
@@ -1,6 +1,9 @@
 package ex20io;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
@@ -36,7 +39,28 @@ public class E11ObjectSerializable {
 			
 			//// 여기까지 실행하면 obj 파일이 생성됨 ////
 			
+			ObjectOutputStream in =
+				new ObjectOutputStream(
+						new FileInputStream(
+								"src/ex20io/circle.obj"));
 			
+			Circle c1 = (Circle)in.readObject();
+			Circle c2 = (Circle)in.readObject();
+			String message = (String)in.readObject();
+			in.close();
+			
+			c1.showCircleInfo();
+			c2.showCircleInfo();
+			System.out.println("String 오브젝트 : "+ message);
+		}
+		catch(ClassNotFoundException e) {
+			System.out.println("[예외]클래스 없음");
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("[예외]파일 없음");
+		}
+		catch (IOException e) {
+			System.out.println("[Exception]뭔가 없음");
 		}
 	}
 }
